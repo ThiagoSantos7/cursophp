94bf3dfbaa91e927a863c7d5d2acd1650404539b
ThiagoSantos7 <seu-email>
2025-12-08 20:35:12 -0300

    Refazendo projeto para solidez de conhecimentos

--- desafios/df11/index.php
+++ desafios/df11/index.php
@@ -4,43 +4,48 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles.css">
+    <link rel="stylesheet" href="./style.css">
     <title>Reajustador de preços</title>
 </head>
 
 <body>
     <?php
-    $preço = $_GET["preco"] ?? 0;
-    $percentual = $_GET["percentual"] ?? 0;
 
-    $resultado = $preço + ($preço * ($percentual / 100));
-    $padrao = numfmt_create("pt-BR", NumberFormatter::CURRENCY);
+        $preco = $_GET["preco"] ?? 0;
+        $percentual = $_GET["percentual"] ?? 0;
+        $result = $preco + $preco * ($percentual / 100);
+
+        $padrao = numfmt_create("pt-BR", NumberFormatter::CURRENCY);
+
     ?>
     <main>
         <section>
-            <h2>Reajustador de preços</h2>
             <form action="<?= $_SERVER["PHP_SELF"] ?>" method="get">
-                <label for="preco">Preço do produto (R$)</label>
-                <input type="number" name="preco" id="idpreco" step="0.01">
-                <label for="percentual" id="percentual">Qual será o percentual: 0%</label>
-                <input name="percentual" id="range" type="range" min="0" max="100">
+                <label for="preco">Preço do produto(R$)</label>
+                <input type="number" name="preco" id="idpreco">
+                <label for="percentual">Qual será o percentual de reajuste? ( <span id="valorPorcentagem">
+                        0</span>%)</label>
+                <input type="range" name="percentual" id="idpercentual">
                 <input class="btn" type="submit" value="Calcular">
             </form>
         </section>
         <br>
         <section>
-            <h2>Resultado do Reajuste</h2>
             <form>
                 <?php
-                echo "<p>O produto custava <strong>" . numfmt_format_currency($padrao, $preço, "BRL") . "</strong>, com $percentual% de aumento vai passar a custar " . numfmt_format_currency($padrao, $resultado, "BRL") . " a partir de agora.</p>"
+                echo "<p>O produto que custava " . numfmt_format_currency($padrao, $preco, "BRL") . " com aumento de <strong>$percentual%</strong> vai passar a custar " . numfmt_format_currency($padrao, $result, "BRL") . " a partir de agora.</p>"
                 ?>
             </form>
         </section>
     </main>
+
     <script>
-        document.getElementById("range").addEventListener("input", function() {
-            document.getElementById("percentual").textContent = "Qual será o percentual: " + this.value + "%";
-        });
+    const range = document.getElementById("idpercentual");
+    const label = document.getElementById("valorPorcentagem");
+
+    range.addEventListener("input", function() {
+        label.textContent = this.value;
+    });
     </script>
 </body>
 
