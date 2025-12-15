017689fb683e1b1f9d1bc2d515c6ce8492da9ff4
ThiagoSantos7 <seu-email>
2025-12-15 18:36:10 -0300

    Refazendo mais um projeto para consolidação de aprendizado

--- desafios/df11/index.php
+++ desafios/df11/index.php
@@ -10,44 +10,41 @@
 
 <body>
     <?php
-
-    $preco = $_GET["preco"] ?? 0;
-    $percentual = $_GET["percentual"] ?? 0;
-    $result = $preco + $preco * ($percentual / 100);
-
-    $padrao = numfmt_create("pt-BR", NumberFormatter::CURRENCY);
-
+    $preco = $_REQUEST["preco"] ?? 0;
+    $perc = $_REQUEST["perc"] ?? 0;
     ?>
     <main>
         <section>
+            <h1>Reajustador de Preços</h1>
             <form action="<?= $_SERVER["PHP_SELF"] ?>" method="get">
-                <label for="preco">Preço do produto(R$)</label>
-                <input type="number" name="preco" id="idpreco" step="0.01">
-                <label for="percentual">Qual será o percentual de reajuste? ( <span id="valorPorcentagem">
-                        0</span>%)</label>
-                <input type="range" name="percentual" id="idpercentual">
-                <input class="btn" type="submit" value="Calcular">
+                <label for="preco">Digite o preço para reajuste</label>
+                <input type="number" name="preco" id="preco" step="0.01" value="<?= $preco ?>">
+                <label for="perc">Quanto por cento você quer aumentar? (<span id="p">?</span>%)</label>
+                <input type="range" name="perc" id="percentual" min="0" max="100" step="1" oninput="aumentoValor()">
+                <input class="btn" type="submit" value="Calcular aumento">
             </form>
         </section>
         <br>
         <section>
+            <?php
+            $porcentagem = ($preco * $perc) / 100;
+            $result = $preco + $porcentagem;
+            ?>
+            <h2>Resultado</h2>
             <form>
-                <?php
-                echo "<p>O produto que custava " . numfmt_format_currency($padrao, $preco, "BRL") .
-                    " com aumento de <strong>$percentual%</strong> vai passar a custar "
-                    . numfmt_format_currency($padrao, $result, "BRL") . " a partir de agora.</p>"
-                ?>
+                <p>O valor R$<?= number_format($preco, 2, ',', '.') ?> com o reajuste de
+                    <?= $perc ?>% fica em R$<?= number_format($result, 2, ',', '.') ?>
+                </p>
+
             </form>
         </section>
     </main>
-
     <script>
-        const range = document.getElementById("idpercentual");
-        const label = document.getElementById("valorPorcentagem");
+    aumentoValor()
 
-        range.addEventListener("input", function() {
-            label.textContent = this.value;
-        });
+    function aumentoValor() {
+        p.innerText = percentual.value
+    }
     </script>
 </body>
 
