234b289e3b3fb3bb20fcfeaf2f340ac7dab84afc
ThiagoSantos7 <seu-email>
2025-12-15 17:09:45 -0300

    Refazendo mais um desafio para consolidação de aprendizado

--- desafios/df10/index.php
+++ desafios/df10/index.php
@@ -4,36 +4,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
-    <title>Calculo de idade</title>
+    <link rel="stylesheet" href="styles.css">
+    <title>Maquina do tempo</title>
 </head>
 
 <body>
-    <?php
-    $nascimento = $_GET["nasc"] ?? 0;
-    $anoE = $_GET["calc"] ?? 0;
-    $calc = $anoE - $nascimento;
-
-    ?>
     <main>
+        <?php
+        $atual = date("Y");
+        $nasc = $_GET["nasc"] ?? '2000';
+        $anoC = $_GET["anoC"] ?? $atual;
+        ?>
         <section>
-            <h2>Calculando a sua idade</h2>
-            <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
-                <label for="nasc">Em que ano você nasceu?</label>
-                <input type="number" name="nasc" id="idnasc">
-                <label class="lab_calc" for="calc">Quer saber sua idade em que ano?(atualmente estamos em
-                    <strong>2025</strong>)</label>
-                <input type="number" name="calc">
-                <input class="btn" type="submit" value="calcular">
+            <h2>Conversor de anos</h2>
+            <form action="<?= $_SERVER["PHP_SELF"] ?>" method="get">
+                <label for="nasc">Digite o ano em que você nasceu</label>
+                <input type="number" name="nasc" min="1900" max="<?= $atual - 1 ?>" value="<?= $nasc ?>">
+                <label for="anoC">Quer saber sua idade em que ano?(atualmente estamos em <?= $atual ?>)</label>
+                <input type="number" name="anoC" value="<?= $anoC ?>">
+                <input class="btn" type="submit" value="Calcular">
             </form>
         </section>
         <br>
         <section>
-            <h2>Resultado</h2>
+            <h2>Resultado da conversão</h2>
             <form>
                 <?php
-                echo "<p>Quem nasceu em $nascimento vai ter <strong>$calc</strong> anos em $anoE</p>"
+                $result = $anoC - $nasc;
                 ?>
+                <ul>
+                    <li>Quem nasceu em <strong>
+                            <?= $nasc ?></strong> em <strong><?= $anoC ?></strong> terá
+                        <strong><?= $result ?></strong>
+                        anos!
+                    </li>
+                </ul>
             </form>
         </section>
     </main>
